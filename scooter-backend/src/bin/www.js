42d72892adf8e71cf062febdb9f6e4af8b12693e
Fermin Alvarez <ferminalvarez.dev@gmail.com>
2024-06-05 14:38:56 -0300

    Config and run reslint and prettier - Backend

--- scooter-backend/src/bin/www.js
+++ scooter-backend/src/bin/www.js
@@ -1,16 +1,16 @@
-const app = require ('../app');
-const swaggerDocs = require('../utils/swagger');
+const app = require("../app");
+const swaggerDocs = require("../utils/swagger");
 
-async function main (){
-    try {        
-        app.listen(process.env.PORT_HOST);
+async function main() {
+  try {
+    app.listen(process.env.PORT_HOST);
 
-        swaggerDocs(app, process.env.PORT_HOST);
+    swaggerDocs(app, process.env.PORT_HOST);
 
-        console.log("Served started on PORT:"+process.env.PORT_HOST);
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
+    console.log("Served started on PORT:" + process.env.PORT_HOST);
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 }
 
 main();
